707f8b896cb4a7ff754064fbc434289ce6da09c1
Paulo Alves <paulo.alves@quero.com>
2020-01-06 23:09:04 -0300

    refactoring basic module

--- main.tf
+++ main.tf
@@ -2,8 +2,12 @@ resource "aws_instance" "main" {
   ami                    = "${var.ami}"
   instance_type          = "${var.instance_type}"
   key_name               = "${var.key_name}"
-  vpc_security_group_ids = "${var.security_groups_id}"
+  root_block_device      = "${var.root_block_device}"
   subnet_id              = "${var.subnet_id}"
   user_data              = "${var.user_data}"
-  root_block_device      = "${var.root_block_device}"
+  vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
+
+  tags = {
+    Name = "${var.name}"
+  }
 }
