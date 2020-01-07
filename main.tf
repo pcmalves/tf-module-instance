5b52b23d878f8815a4fdc381f8549230b97d32ec
Paulo Alves <paulo.alves@quero.com>
2020-01-06 21:56:14 -0300

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "main" {
-  ami               = "${var.ami}"
-  instance_type     = "${var.instance_type}"
-  key_name          = "${var.key_name}"
-  vpc_security_group_ids   = "${var.security_groups_id}"
-  subnet_id         = "${var.subnet_id}"
-  user_data         = "${var.user_data}"
-  root_block_device = "${var.root_block_device}"
+  ami                    = "${var.ami}"
+  instance_type          = "${var.instance_type}"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = "${var.security_groups_id}"
+  subnet_id              = "${var.subnet_id}"
+  user_data              = "${var.user_data}"
+  root_block_device      = "${var.root_block_device}"
 }
