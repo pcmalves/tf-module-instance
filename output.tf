7105bf0193ef270d4a5f8d835e2d97220ea56e07
Paulo Alves <paulo.alves@quero.com>
2020-01-06 21:44:03 -0300

    only the necessary

--- output.tf
+++ output.tf
@@ -6,14 +6,6 @@ output "private_ip" {
   value = "${aws_instance.main.private_ip}"
 }
 
-output "placement_group" {
-  value = "${aws_instance.main.placement_group}"
-}
-
-output "key_name" {
-  value = "${aws_instance.main.key_name}"
-}
-
 output "public_dns" {
   value = "${aws_instance.main.public_dns}"
 }
@@ -24,12 +16,4 @@ output "public_ip" {
 
 output "private_dns" {
   value = "${aws_instance.main.private_dns}"
-}
-
-output "security_groups" {
-  value = "${aws_instance.main.security_groups}"
-}
-
-output "subnet_id" {
-  value = "${aws_instance.main.subnet_id}"
 }
---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "ami" {}
+
+variable "instance_type" {}
+
+variable "key_name" {}
+
+variable "security_groups" {}
+
+variable "subnet_id" {}
+
+variable "user_data" {}
+
+variable "root_block_device" {}
