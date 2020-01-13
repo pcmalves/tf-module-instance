6aab57ab8f916f958dcc8c8ce30239103aa89b0b
Paulo Cezar Martins Alves <paulo.alves@redealumni.com>
2020-01-13 20:01:56 -0300

    alter file variable

--- variable.tf
+++ variable.tf
@@ -1,12 +1,12 @@
-type = "list"
-
 variable "ami" {}
 variable "instance_type" {}
 variable "key_name" {}
 variable "name_instance" {}
 
 variable "root_block_device" {
-  variable "subnet_id"{}
-  variable "user_data"{}
-  variable "vpc_security_group_ids"{}
+  type = "list"
 }
+
+variable "subnet_id" {}
+variable "user_data" {}
+variable "vpc_security_group_ids" {}
